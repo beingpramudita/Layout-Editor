e7dad353f727e8af349a2f19d8c0aa1a072121f2
beingpramudita <pramudita644@gmail.com>
2022-03-22 08:20:05 +0545

    Saved Instance

--- app/src/main/java/com/example/hello_toast_dma/MainActivity.java
+++ app/src/main/java/com/example/hello_toast_dma/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.hello_toast_dma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private int mCount=0;
+    private static final String TAG=MainActivity.class.getSimpleName();
     private TextView mShowCount;
 
     @Override
@@ -17,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        if(savedInstanceState !=null){
+            mCount=savedInstanceState.getInt(TAG,0);
+            mShowCount.setText(""+mCount);
+        }
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(TAG, mCount);
+    }
+
     public void showToast(View view) {
         Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
         toast.show();
